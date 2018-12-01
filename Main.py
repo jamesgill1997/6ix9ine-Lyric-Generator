b08a217ee43bbeae6839e74afc214d7dc3c495cd
James Gill <jamesgill1997@hotmail.com>
2018-12-01 11:00:45 +0000

    Reformatted the file so all functions are in one file, one take baby. TR3YWAY

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from SongName import *
+import random
 
 # This is a program to create bespoke songs using the style of Tekashi 6ix9ine
 # In the form of:
@@ -9,3 +9,23 @@ from SongName import *
 #               -- chorus
 #               -- outro
 
+
+def create_finished_song():
+    # Function to put together all the elements for your finished 6ix9ine Masterpiece
+    create_song_name()
+
+
+def create_song_name():
+    # Function to make the song title
+    # 4 letter words, all caps, consonants characters 1 and 3, vowels 2 and 4, vowels same on both
+    # Populating lists of consonants and vowels
+    consonants = ("B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z")
+    vowels = ("A", "E", "I", "O", "U", "Y")
+
+    # Create the song name
+    repeated_vowel = random.choice(vowels)
+    songname = random.choice(consonants) + repeated_vowel + random.choice(consonants) + repeated_vowel
+    print("Your song title is \"" + songname + "\"")
+
+
+create_finished_song()
