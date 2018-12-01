36546f4d532c348699e623d570dec684b9ce03e9
James Gill <jamesgill1997@hotmail.com>
2018-12-01 11:56:28 +0000

    Added five total ascii art pieces for the console

--- Main.py
+++ Main.py
@@ -28,28 +28,11 @@ import random
 #  \______/  \______/  \______/ |__/     |__/     \______/ |__/  |__/|__/  \__/ \______/
 
 
-def create_finished_song():
-    # Function to put together all the elements for your finished 6ix9ine Masterpiece
-    create_song_name()
-
-
-def create_song_name():
-    # Function to make the song title
-    # 4 letter words, all caps, consonants characters 1 and 3, vowels 2 and 4, vowels same on both
-    # Populating lists of consonants and vowels
-    consonants = ("B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z")
-    vowels = ("A", "E", "I", "O", "U")
-
-    # Create the song name
-    repeated_vowel = random.choice(vowels)
-    songname = random.choice(consonants) + repeated_vowel + random.choice(consonants) + repeated_vowel
-    print("Your song title is \"" + songname + "\"")
-
-
 def print_ascii_art():
+    # Function to add sick ascii art in the console
     treyway = ("""\
-      /$$$$$$$$ /$$$$$$$   /$$$$$$  /$$     /$$ /$$      /$$  /$$$$$$  /$$     /$$
-     |__  $$__/| $$__  $$ /$$__  $$|  $$   /$$/| $$  /$ | $$ /$$__  $$|  $$   /$$/
+     /$$$$$$$$ /$$$$$$$   /$$$$$$  /$$     /$$ /$$      /$$  /$$$$$$  /$$     /$$
+    |__  $$__/| $$__  $$ /$$__  $$|  $$   /$$/| $$  /$ | $$ /$$__  $$|  $$   /$$/
        | $$   | $$  \ $$|__/  \ $$ \  $$ /$$/ | $$ /$$$| $$| $$  \ $$ \  $$ /$$/
        | $$   | $$$$$$$/   /$$$$$/  \  $$$$/  | $$/$$ $$ $$| $$$$$$$$  \  $$$$/
        | $$   | $$__  $$  |___  $$   \  $$/   | $$$$_  $$$$| $$__  $$   \  $$/
@@ -67,8 +50,58 @@ def print_ascii_art():
      |  $$$$$$/|  $$$$$$/|  $$$$$$/| $$ \/  | $$    |  $$$$$$/| $$  | $$| $$ \  $$|  $$$$$$/
       \______/  \______/  \______/ |__/     |__/     \______/ |__/  |__/|__/  \__/ \______/
                         """)
-    ascii_options = (treyway, scumgang)
+    gangshit = ("""\
+      /$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$       /$$$$$$  /$$   /$$ /$$$$$$ /$$$$$$$$
+     /$$__  $$ /$$__  $$| $$$ | $$ /$$__  $$     /$$__  $$| $$  | $$|_  $$_/|__  $$__/
+    | $$  \__/| $$  \ $$| $$$$| $$| $$  \__/    | $$  \__/| $$  | $$  | $$     | $$   
+    | $$ /$$$$| $$$$$$$$| $$ $$ $$| $$ /$$$$    |  $$$$$$ | $$$$$$$$  | $$     | $$   
+    | $$|_  $$| $$__  $$| $$  $$$$| $$|_  $$     \____  $$| $$__  $$  | $$     | $$   
+    | $$  \ $$| $$  | $$| $$\  $$$| $$  \ $$     /$$  \ $$| $$  | $$  | $$     | $$   
+    |  $$$$$$/| $$  | $$| $$ \  $$|  $$$$$$/    |  $$$$$$/| $$  | $$ /$$$$$$   | $$   
+     \______/ |__/  |__/|__/  \__/ \______/      \______/ |__/  |__/|______/   |__/   
+                """)
+    splishsplash = ("""\
+      /$$$$$$  /$$$$$$$  /$$       /$$$$$$  /$$$$$$  /$$   /$$      /$$$$$$  /$$$$$$$  /$$        /$$$$$$   /$$$$$$  /$$   /$$
+     /$$__  $$| $$__  $$| $$      |_  $$_/ /$$__  $$| $$  | $$     /$$__  $$| $$__  $$| $$       /$$__  $$ /$$__  $$| $$  | $$
+    | $$  \__/| $$  \ $$| $$        | $$  | $$  \__/| $$  | $$    | $$  \__/| $$  \ $$| $$      | $$  \ $$| $$  \__/| $$  | $$
+    |  $$$$$$ | $$$$$$$/| $$        | $$  |  $$$$$$ | $$$$$$$$    |  $$$$$$ | $$$$$$$/| $$      | $$$$$$$$|  $$$$$$ | $$$$$$$$
+     \____  $$| $$____/ | $$        | $$   \____  $$| $$__  $$     \____  $$| $$____/ | $$      | $$__  $$ \____  $$| $$__  $$
+     /$$  \ $$| $$      | $$        | $$   /$$  \ $$| $$  | $$     /$$  \ $$| $$      | $$      | $$  | $$ /$$  \ $$| $$  | $$
+    |  $$$$$$/| $$      | $$$$$$$$ /$$$$$$|  $$$$$$/| $$  | $$    |  $$$$$$/| $$      | $$$$$$$$| $$  | $$|  $$$$$$/| $$  | $$
+     \______/ |__/      |________/|______/ \______/ |__/  |__/     \______/ |__/      |________/|__/  |__/ \______/ |__/  |__/
+                """)
+    schmurder = ("""\
+      /$$$$$$   /$$$$$$  /$$   /$$ /$$      /$$ /$$   /$$ /$$$$$$$  /$$$$$$$  /$$$$$$$$ /$$$$$$$ 
+     /$$__  $$ /$$__  $$| $$  | $$| $$$    /$$$| $$  | $$| $$__  $$| $$__  $$| $$_____/| $$__  $$
+    | $$  \__/| $$  \__/| $$  | $$| $$$$  /$$$$| $$  | $$| $$  \ $$| $$  \ $$| $$      | $$  \ $$
+    |  $$$$$$ | $$      | $$$$$$$$| $$ $$/$$ $$| $$  | $$| $$$$$$$/| $$  | $$| $$$$$   | $$$$$$$/
+     \____  $$| $$      | $$__  $$| $$  $$$| $$| $$  | $$| $$__  $$| $$  | $$| $$__/   | $$__  $$
+     /$$  \ $$| $$    $$| $$  | $$| $$\  $ | $$| $$  | $$| $$  \ $$| $$  | $$| $$      | $$  \ $$
+    |  $$$$$$/|  $$$$$$/| $$  | $$| $$ \/  | $$|  $$$$$$/| $$  | $$| $$$$$$$/| $$$$$$$$| $$  | $$
+     \______/  \______/ |__/  |__/|__/     |__/ \______/ |__/  |__/|_______/ |________/|__/  |__/
+                """)
+    ascii_options = (treyway, scumgang, gangshit, splishsplash, schmurder)
     print(random.choice(ascii_options))
 
-create_finished_song()
+
+def create_finished_song():
+    # Function to put together all the elements for your finished 6ix9ine Masterpiece
+    create_song_name()
+
+
+def create_song_name():
+    # Function to make the song title
+    # 4 letter words, all caps, consonants characters 1 and 3, vowels 2 and 4, vowels same on both
+    # Populating lists of consonants and vowels
+    consonants = ("B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z")
+    vowels = ("A", "E", "I", "O", "U")
+
+    # Create the song name
+    repeated_vowel = random.choice(vowels)
+    songname = random.choice(consonants) + repeated_vowel + random.choice(consonants) + repeated_vowel
+    print("Your song title is: \"" + songname + "\"")
+
+
 print_ascii_art()
+create_finished_song()
+
