2979dfabdf959d1ba900487b3222e8c9ac8711fa
James Gill <jamesgill1997@hotmail.com>
2018-12-01 10:40:05 +0000

    Initial commit, completed song name generator and creation of main file

---
+++ Main.py
@@ -0,0 +1,11 @@
+from SongName import *
+
+# This is a program to create bespoke songs using the style of Tekashi 6ix9ine
+# In the form of:
+#               -- intro
+#               -- verse 1
+#               -- chorus
+#               -- verse 2 (feature)
+#               -- chorus
+#               -- outro
+
