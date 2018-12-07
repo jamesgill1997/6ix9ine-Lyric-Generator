fa88b5a07805c04a7fbeff5060973842b10c9b4b
James Gill <jamesgill1997@hotmail.com>
2018-12-07 16:45:43 +0000

    Adding the ability for the user to decide the number of artists used and which, in a basic way that will be improved upon in time. Added 3 todo's and cleaned up comments

--- general/general_song_creator_p3.py
+++ general/general_song_creator_p3.py
@@ -1,22 +1,44 @@
-import random
 import lyricsgenius as genius
 import json
 import markovify
 import os
 
-artist_one = 'Coldplay'
-artist_two = 'Avicii'
+# TODO replace lyricsgenius with Genius' API + web scraper
+# TODO implement flask wrapper to provide inputs
+# TODO more front end development
+
+# Allowing user inputs to decide how many artists will be used when making the album
+num_artists = int(input("How many artists to combine?"))
+
+# Initialising variables and lists
+artists = []
+i = 0
+
+# Adding user inputted artists to a list of size num_artists
+while num_artists > 0:
+    current_artist = i + 1
+    artists.append(input("Who is artist " + str(current_artist) + "?"))
+    num_artists = num_artists - 1
+    i = i + 1
+
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+
+
+artist_one = '21 Savage'
+artist_two = 'Drake'
 num_songs = 10
 
 dash = '-'*60
 
-## Finding Songs by Artist
+# Finding Songs by Artist
 
 api = genius.Genius('uDarA9_cUEi8NgSiEliFR8Lu-TRl-sanYEfw1_fDNLRyejlgXDul2-QfYJZ4SvQF')
 artist_1 = api.search_artist(artist_one, max_songs=num_songs)
 artist_2 = api.search_artist(artist_two, max_songs=num_songs)
 
-## Saving Song Lyrics to JSON
+# Saving Song Lyrics to JSON
 
 artist_1.save_lyrics()
 artist_2.save_lyrics()
@@ -27,7 +49,7 @@ with open('Lyrics_{}.json'.format(artist_one.replace(' ','')), 'r') as fp:
 with open('Lyrics_{}.json'.format(artist_two.replace(' ','')), 'r') as fp:
     obj_2 = json.load(fp)
 
-## Saving JSON Data to Text File
+# Saving JSON Data to Text File
 
 with open('all_lyrics_1' + '.txt', 'a') as the_file:
     for i in range(len(obj_1['songs'])):
@@ -37,19 +59,19 @@ with open('all_lyrics_2' + '.txt', 'a') as the_file:
     for i in range(len(obj_2['songs'])):
         the_file.write(obj_2['songs'][i]['lyrics'])
 
-## Opening Text Files
+# Opening Text Files
 
 text_1 = open("all_lyrics_1.txt").read()
 text_2 = open("all_lyrics_2.txt").read()
 
-## Creating Markov Models
+# Creating Markov Models
 
 model_1 = markovify.NewlineText(text_1, state_size=2)
 model_2 = markovify.NewlineText(text_2, state_size=2)
 
 combined_model = markovify.combine([model_1, model_2])
 
-## Printing Final Song
+# Printing Final Song
 
 album_name = 'album_one'
 
