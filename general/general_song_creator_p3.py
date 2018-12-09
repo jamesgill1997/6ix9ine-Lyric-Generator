60ef2841037260bde050fae8c6f60fae619c59e2
Daniel Kelshaw <dkelsh.main@gmail.com>
2018-12-09 01:14:26 +0000

    Added functionality for the combining of markov chains Added the capability to create an album from the chains The album can be named by the user + the program will check if that album already exists or not Added some TODOs that will need to be looked at
    Check if the commented second half of the program is still necessary - if not delete.

--- general/general_song_creator_p3.py
+++ general/general_song_creator_p3.py
@@ -58,70 +58,36 @@ for count, i in enumerate(file_list):
 
 # Creates markov chain models for each of the open files
 markov_models = {}
+list_models = []
 for count, i in enumerate(open_files):
     markov_models['model_{}'.format(count)] = markovify.NewlineText(i, state_size=2)
 
-
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-
-
-artist_one = 'Kanye West'
-artist_two = 'Drake'
-num_songs = 10
+# Combines all markov chains
+# TODO Need to make user input for whether they want a mixturer of artists in one go, or just as features
+combined_model = markovify.combine(list(markov_models.values()))
 
 
-# Finding Songs by Artist
+# ALBUM SECTION
 
-api = genius.Genius('uDarA9_cUEi8NgSiEliFR8Lu-TRl-sanYEfw1_fDNLRyejlgXDul2-QfYJZ4SvQF')
-artist_1 = api.search_artist(artist_one, max_songs=num_songs)
-artist_2 = api.search_artist(artist_two, max_songs=num_songs)
+# Asks for inputs regarding the album
+album_name = input('What is the album name: ')
+num_songs_album = input('How many songs in the album: ')
 
-# Saving Song Lyrics to JSON
+# Checks if the album already exists or not
+album_bool = True
+while album_bool:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.exists(album_name):
+        os.makedirs(album_name)
+        album_bool = False
+    else:
+        print('This album already exists.')
+        album_name = input('What is the album name: ')
 
-artist_1.save_lyrics()
-artist_2.save_lyrics()
-
-with open('Lyrics_{}.json'.format(artist_one.replace(' ','')), 'r') as fp:
-    obj_1 = json.load(fp)
-
-with open('Lyrics_{}.json'.format(artist_two.replace(' ','')), 'r') as fp:
-    obj_2 = json.load(fp)
-
-# Saving JSON Data to Text File
-
-with open('all_lyrics_1' + '.txt', 'a') as the_file:
-    for i in range(len(obj_1['songs'])):
-        the_file.write(obj_1['songs'][i]['lyrics'])
-
-with open('all_lyrics_2' + '.txt', 'a') as the_file:
-    for i in range(len(obj_2['songs'])):
-        the_file.write(obj_2['songs'][i]['lyrics'])
-
-# Opening Text Files
-
-text_1 = open("all_lyrics_1.txt").read()
-text_2 = open("all_lyrics_2.txt").read()
-
-# Creating Markov Models
-
-model_1 = markovify.NewlineText(text_1, state_size=2)
-model_2 = markovify.NewlineText(text_2, state_size=2)
-
-combined_model = markovify.combine([model_1, model_2])
-
-# Printing Final Song
-
-album_name = 'album_one'
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-if not os.path.exists(album_name):
-    os.makedirs(album_name)
-
-
-for i in range(8):
-    with open(album_name + "/" + 'song_{}'.format(i) + '.txt', 'a') as the_file:
+# Generates the songs with a specific template and adds them into the album
+# TODO Make a nicer format for the printing of the song to the file
+for i in range(num_songs_album):
+    with open(album_name + "/" + 'Song_{}'.format(i+1) + '.txt', 'a') as the_file:
         the_file.write('VERSE ONE')
         the_file.write('\n')
         the_file.write(dash)
@@ -153,4 +119,95 @@ for i in range(8):
             the_file.write('\n')
 
 
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
 
+
+# artist_one = 'Kanye West'
+# artist_two = 'Drake'
+# num_songs = 10
+#
+#
+# # Finding Songs by Artist
+#
+# api = genius.Genius('uDarA9_cUEi8NgSiEliFR8Lu-TRl-sanYEfw1_fDNLRyejlgXDul2-QfYJZ4SvQF')
+# artist_1 = api.search_artist(artist_one, max_songs=num_songs)
+# artist_2 = api.search_artist(artist_two, max_songs=num_songs)
+#
+# # Saving Song Lyrics to JSON
+#
+# artist_1.save_lyrics()
+# artist_2.save_lyrics()
+#
+# with open('Lyrics_{}.json'.format(artist_one.replace(' ','')), 'r') as fp:
+#     obj_1 = json.load(fp)
+#
+# with open('Lyrics_{}.json'.format(artist_two.replace(' ','')), 'r') as fp:
+#     obj_2 = json.load(fp)
+#
+# # Saving JSON Data to Text File
+#
+# with open('all_lyrics_1' + '.txt', 'a') as the_file:
+#     for i in range(len(obj_1['songs'])):
+#         the_file.write(obj_1['songs'][i]['lyrics'])
+#
+# with open('all_lyrics_2' + '.txt', 'a') as the_file:
+#     for i in range(len(obj_2['songs'])):
+#         the_file.write(obj_2['songs'][i]['lyrics'])
+#
+# # Opening Text Files
+#
+# text_1 = open("all_lyrics_1.txt").read()
+# text_2 = open("all_lyrics_2.txt").read()
+#
+# # Creating Markov Models
+#
+# model_1 = markovify.NewlineText(text_1, state_size=2)
+# model_2 = markovify.NewlineText(text_2, state_size=2)
+#
+# combined_model = markovify.combine([model_1, model_2])
+#
+# # Printing Final Song
+#
+# album_name = 'album_one'
+#
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# if not os.path.exists(album_name):
+#     os.makedirs(album_name)
+#
+#
+# for i in range(8):
+#     with open(album_name + "/" + 'song_{}'.format(i) + '.txt', 'a') as the_file:
+#         the_file.write('VERSE ONE')
+#         the_file.write('\n')
+#         the_file.write(dash)
+#         the_file.write('\n')
+#         for i in range(4):
+#             the_file.write(combined_model.make_sentence(tries=150))
+#             the_file.write('\n')
+#
+#         the_file.write('\n')
+#
+#         the_file.write('VERSE TWO')
+#         the_file.write('\n')
+#         the_file.write(dash)
+#         the_file.write('\n')
+#
+#         for i in range(8):
+#             the_file.write(combined_model.make_sentence(tries=150))
+#             the_file.write('\n')
+#
+#         the_file.write('\n')
+#
+#         the_file.write('VERSE THREE')
+#         the_file.write('\n')
+#         the_file.write(dash)
+#         the_file.write('\n')
+#
+#         for i in range(4):
+#             the_file.write(combined_model.make_sentence(tries=150))
+#             the_file.write('\n')
+#
+#
+#
